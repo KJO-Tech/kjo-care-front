421fccc7d5efef7c98d2757535bebf5bff2dcb11
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2025-10-10 02:13:36 -0500

    fix(keycloak): change init function and refresh session automatically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { KeycloakService } from './modules/auth/services/keycloak.service';
 import { ToastComponent } from './shared/components/layout/toast/toast.component';
+import { ThemeControllerComponent } from './shared/components/layout/theme-controller/theme-controller.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastComponent],
+  imports: [RouterOutlet, ToastComponent, ThemeControllerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,6 +15,10 @@ export class AppComponent {
 
   login() {
     this.keycloakService.login();
+  }
+
+  register() {
+    this.keycloakService.register();
   }
 
   ngOnInit() {
