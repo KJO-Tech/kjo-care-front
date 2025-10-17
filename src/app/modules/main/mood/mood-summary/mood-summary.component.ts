bd961188ad53dc9803dd7d4733594a31de9155da
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2025-10-17 18:43:33 -0500

    fix(mood): add mood summary page

--- src/app/modules/main/mood/mood-summary/mood-summary.component.ts
+++ src/app/modules/main/mood/mood-summary/mood-summary.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { WeeklyHistoryComponent } from '../../home/components/weekly-history/weekly-history.component';
 
 @Component({
   selector: 'mood-summary',
-  imports: [],
-  templateUrl: './mood-summary.component.html',
+  imports: [
+    WeeklyHistoryComponent
+  ],
+  templateUrl: './mood-summary.component.html'
 })
 export default class MoodSummaryComponent {
-
 }
