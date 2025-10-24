568ca3159edc63db5e86b3a40fc8079011249425
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2025-10-23 19:24:21 -0500

    feat(resources): add emergency resources page fot user

--- src/app/modules/main/resources/resources-emergency/resources-emergency.component.ts
+++ src/app/modules/main/resources/resources-emergency/resources-emergency.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { EmergencyResourceService } from '../../../../core/services/emergency-resource.service';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'resources-emergency',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export default class ResourcesEmergencyComponent {
 
+  private resourcesService = inject(EmergencyResourceService)
+
+  resources = rxResource({
+    loader: () => this.resourcesService.getAllActive().pipe(
+      map(response => response.result)
+    )
+  })
+
+  copyToClipboard(text: string) {
+    navigator.clipboard.writeText(text);
+  }
+
+  reload() {
+    this.resources.reload();
+  }
 }
