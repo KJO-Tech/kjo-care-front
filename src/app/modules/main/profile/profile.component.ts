eace54625f6153fb4b9a17b2325bbc9b93aee6a8
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2025-10-25 16:36:41 -0500

    fix(profile): add profile page

--- src/app/modules/main/profile/profile.component.ts
+++ src/app/modules/main/profile/profile.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { KeycloakService } from '../../auth/services/keycloak.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
 })
 export default class ProfileComponent {
+  private keycloakService = inject(KeycloakService);
 
+  userProfile = this.keycloakService.profile;
+
+  editProfile(): void {
+    this.keycloakService.goToAccountManagement();
+  }
+
+  logout(): void {
+    this.keycloakService.logout();
+  }
 }
