c622dc8ccc6902867488f1c594ffa8091c39a896
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2025-10-25 20:33:07 -0500

    feat(home): get stats from analytics service

--- src/app/core/services/analytics.service.ts
+++ src/app/core/services/analytics.service.ts
@@ -4,18 +4,20 @@ import { environment } from '../../../environments/environment';
 import { catchError, Observable, tap, throwError } from 'rxjs';
 import { MoodAnalyticsResponse, MoodTrendsAnalysis } from '../interfaces/mood-analytics.response';
 import { ApiResponse } from '../../shared/interfaces/api-response';
+import { DashboardSummary, Summary } from '../interfaces/analytics.interface';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AnalyticsService {
   private http = inject(HttpClient);
   private readonly baseUrl = `${environment.apiUrl}/api/mind/mood-tracking`;
+  private readonly analyticsUrl = `${environment.apiUrl}/api/mind/analytics`;
 
   getMoodAnalytics(months: number = 3): Observable<ApiResponse<MoodAnalyticsResponse>> {
     return this.http
       .get<ApiResponse<MoodAnalyticsResponse>>(
-        `${this.baseUrl}/user-mood/statistics?month=${months}`,
+        `${this.baseUrl}/user-mood/statistics?month=${months}`
       )
       .pipe(
         tap((response) => {
@@ -26,23 +28,30 @@ export class AnalyticsService {
           return throwError(
             () =>
               new Error(
-                'Error la obtener las estadisticas de los estados de animo',
-              ),
+                'Error la obtener las estadisticas de los estados de animo'
+              )
           );
-        }),
+        })
       );
   }
 
   getMoodTrendsAnalysis(month: number = 3): Observable<ApiResponse<MoodTrendsAnalysis>> {
     return this.http.get<ApiResponse<MoodTrendsAnalysis>>(`${this.baseUrl}/user-mood/trends-analysis?months=${month}`
     ).pipe(
-      tap((response) => console.log("Mood Trend Anlysis", response)),
+      tap((response) => console.log('Mood Trend Anlysis', response)),
       catchError((error) => {
-        console.error("Error al traer las estadisticas", error)
-        return throwError(() => new Error("Error al obtener las analiticas de los estados de animo"))
+        console.error('Error al traer las estadisticas', error);
+        return throwError(() => new Error('Error al obtener las analiticas de los estados de animo'));
       })
     );
   }
 
+  getSummary() {
+    return this.http.get<Summary>(`${this.analyticsUrl}/summary`);
+  }
+
+  getHomeStats() {
+    return this.http.get<DashboardSummary>(`${this.analyticsUrl}/summary-dashboard`);
+  }
 
 }
--- src/app/modules/main/home/components/statistics/statistics.component.ts
+++ src/app/modules/main/home/components/statistics/statistics.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { EmergencyResourceService } from '../../../../../core/services/emergency-resource.service';
 import { map } from 'rxjs';
+import { AnalyticsService } from '../../../../../core/services/analytics.service';
+import { MoodTrackingUserService } from '../../../../../core/services/mood-user.service';
 
 @Component({
   selector: 'home-statistics',
@@ -9,16 +11,26 @@ import { map } from 'rxjs';
   templateUrl: './statistics.component.html',
 })
 export class StatisticsComponent {
-  resourceService = inject(EmergencyResourceService);
+  private moodUserService = inject(MoodTrackingUserService);
+  analyticsService = inject(AnalyticsService);
 
   stats = rxResource({
-    loader: () => this.resourceService.getStats().pipe(
-      map(response => response.result)
-    )
+    loader: () => this.analyticsService.getHomeStats()
   });
+
+  moods = rxResource({
+    loader: () => {
+      return this.moodUserService.getMyMoods().pipe(
+        map((response) => response.result)
+      );
+    }
+  });
+
+  countCheckins = computed(() => this.moods.value()?.length ?? 0)
 
   reload() {
     this.stats.reload();
+    this.moods.reload();
   }
 
 }
